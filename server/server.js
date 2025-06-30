73a057045a64f04ed42cbf9bff7c98e4a9b883e0
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-06-30 22:16:58 +0100

    feat: connect to db successfully

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const app = require("./app");
+const db = require("./config/database");
+
 const PORT = process.env.SERVER_PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+db.connectToMongoDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(
+      "Failed to connect to MongoDB and start server:",
+      err.message
+    );
+  });
