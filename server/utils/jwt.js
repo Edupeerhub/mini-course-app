7955202a99c863ef46bdede9ac3240c1d9b02707
MikaTech-dev <verasamoma@gmail.com>
2025-07-03 16:53:00 +0100

    feat: add JWT utility functions for token generation and verification (#44)

---
+++ server/utils/jwt.js
@@ -0,0 +1,17 @@
+const jwt = require ("jsonwebtoken")
+
+// Generating new Token
+const tokenGeneration = (userid) =>{
+    return jwt.sign(userid), process.env.JWT_SECRET_KEY, ({ expiresIn: process.env.JWT_EXPIRES_IN })
+}
+
+// Verifying generated Tokens
+const tokenVerification = (token) => {
+    return jwt.verify (token, process.env.JWT_SECRET_KEY)
+}
+
+// Exports
+module.exports = {
+    tokenGeneration,
+    tokenVerification
+}
