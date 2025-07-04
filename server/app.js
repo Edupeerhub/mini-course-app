59e96cdbd152c4b3c55a545997a0d436e2fc9c98
jamal-o <jamaldeenolasupo@gmail.com>
2025-07-04 18:44:34 +0100

    feature: add error handler for json body parser

--- server/app.js
+++ server/app.js
@@ -1,19 +1,20 @@
 const NODE_ENV = process.env.NODE_ENV || "development";
 const path = require("path");
-require("dotenv").config({ path: path.normalize(`./.env.${NODE_ENV}` ) });
-
+require("dotenv").config({ path: path.normalize(`./.env.${NODE_ENV}`) });
 
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
 const app = express();
 
-const courseRouter  = require("./route/course.route");
+const courseRouter = require("./route/course.route");
 // Middleware
 app.use(logger("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json(), (error, req, res, next) => {
+  res.status(400).send("Bad Request: Invalid Json");
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Course CRUD App!");
