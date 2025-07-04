b10e4cf4028e98c89d1f50c0d873104502da179a
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-07-04 12:21:41 +0100

    feat: add password hash, check and genrate jwt function as mongoose pre-hooks

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
@@ -28,5 +30,23 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.isValidPassword = async function (userPassword) {
+  return bcrypt.compare(userPassword, this.password);
+};
+
+userSchema.methods.generateAuthToken = function () {
+  return jwt.sign(
+    { user: { id: this._id, email: this.email } },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES_IN || "1h" }
+  );
+};
 
 module.exports = mongoose.model("User", userSchema);
