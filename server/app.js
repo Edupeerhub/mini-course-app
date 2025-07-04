737054a9e9fd17718ec1193c08a269b1c5bcd0cc
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-07-04 19:10:21 +0100

    refactor: minor changes to naming and .env.example

--- server/app.js
+++ server/app.js
@@ -1,17 +1,16 @@
 const NODE_ENV = process.env.NODE_ENV || "development";
 const path = require("path");
-require("dotenv").config({ path: path.normalize(`./.env.${NODE_ENV}` ) });
-
+require("dotenv").config({ path: path.normalize(`./.env.${NODE_ENV}`) });
 
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
 const app = express();
 
-const courseRouter  = require("./route/course.route");
+const courseRouter = require("./route/course.route");
 // Middleware
 app.use(logger("dev"));
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,7 +18,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Course CRUD App!");
 });
 
-app.use("/course", courseRouter);
+app.use("/api/course", courseRouter);
 
 //404 handler
 app.use((req, res, next) => {
