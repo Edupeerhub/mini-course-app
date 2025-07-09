8c07bb18b1af85b7aa4c0695e553913ddb8fde7e
jamal-o <jamaldeenolasupo@gmail.com>
2025-07-09 11:09:07 +0100

    feat: authenticate course routes

--- server/route/course.route.js
+++ server/route/course.route.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const courseController = require("../controller/course.controller");
 const {courseValidation} = require("../middleware/course.middleware");
+const {authenticateToken} = require("../middleware/authorize");
 
-
-
+router.use(authenticateToken); 
 router.get("/", courseController.getAllCourses);
 router.get("/:id", courseController.getOneCourse);
 router.post("/", courseValidation, courseController.createCourse);
