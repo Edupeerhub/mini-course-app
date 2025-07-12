07b70b1c5484e82bbd09865e0d7b62ee2b5ed653
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-07-12 01:15:23 +0100

    test git workflow

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,17 +1,21 @@
-const jwt = require ("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 // Generating new Token
-const tokenGeneration = (userid) =>{
-    return jwt.sign(userid), process.env.JWT_SECRET_KEY, ({ expiresIn: process.env.JWT_EXPIRES_IN })
-}
+const tokenGeneration = (userid) => {
+  return (
+    jwt.sign(userid),
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+};
 
-// Verifying generated Tokens
+// Verifying generated Tokens.
 const tokenVerification = (token) => {
-    return jwt.verify (token, process.env.JWT_SECRET_KEY)
-}
+  return jwt.verify(token, process.env.JWT_SECRET_KEY);
+};
 
 // Exports
 module.exports = {
-    tokenGeneration,
-    tokenVerification
-}
+  tokenGeneration,
+  tokenVerification,
+};
